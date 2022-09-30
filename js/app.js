b08e609380784145494f3688f6b3bc4a3c0682c5
Amber Joy <amber.joy.dev@gmail.com>
2022-09-30 00:29:41 -0500

    i dunno let's see if this works for iOS

--- js/app.js
+++ js/app.js
@@ -1,16 +1,68 @@
+const hasMotionDetection = "ondevicemotion" in window;
+const shakeEvent = new Shake({ threshold: 8 });
+
 // LISTENERS
-var shakeEvent = new Shake({ threshold: 8 });
-shakeEvent.start();
-window.addEventListener("shake", () => changeColors(), false);
-
 document.addEventListener("DOMContentLoaded", init, false);
-
-// FUNCTIONS
 function init() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("../service-worker.js");
   }
   ntc.init();
+
+  if (hasMotionDetection) {
+    main.innerHTML = "SHAKE ME";
+  } else {
+    main.innerHTML = "CLICK ME";
+  }
+}
+
+document.body.onclick = () => {
+  // iOS 13+ feature detect
+  if (typeof DeviceMotionEvent.requestPermission === "function") {
+    /**
+     * The name "requestPermission" is a bit of a misnomer.
+     * ... "requestPermission" checks if the permission is set and returns the status.
+     * ONLY if not set does it activate the native request mechanism.
+     * – E. Maggini
+     * Aug 17, 2021 at 14:03
+     * https://stackoverflow.com/questions/68815767/unable-to-check-devicemotionevent-permission-state#comment121622865_68816706
+     */
+    DeviceMotionEvent.requestPermission()
+      .then((permissionState) => {
+        // ig user GRANTS permission, or permisson was ALREADY granted...
+        if (permissionState === "granted") {
+
+          // Check to see is permission was already here or not...
+          if (!("permissionGranted" in localStorage) || localStorage.getItem("permissionGranted") !== "true") {
+            // if it wasn't, change text
+            main.innerHTML = "SHAKE ME";
+          }
+
+          // if it was, do the thing
+          localStorage.setItem("permissionGranted", "true");
+          startShakeEvent();
+
+        } else {
+          // i guess if we can't get permission, just change on click
+          changeColors()
+        }
+      })
+      .catch(console.error);
+  } else {
+    // if this is not an iOS 13+ device,
+    // check for motion. IF not, do the thing because we clicked.
+    // if yes, use the Shake event 
+    if (!hasMotionDetection) {
+      changeColors();
+    }
+    startShakeEvent();
+  }
+};
+
+// FUNCTIONS
+function startShakeEvent() {
+  shakeEvent.start();
+  window.addEventListener("shake", () => changeColors(), false);
 }
 
 function changeColors() {
@@ -38,10 +90,3 @@ function changeColors() {
 function isVowel(c) {
   return ["a", "e", "i", "o", "u"].indexOf(c.toLowerCase()) !== -1;
 }
-
-// allow click to change color if no motion detection available on this device
-// const hasMotionDetection = "ondevicemotion" in window;
-document.body.onclick = () => {
-  // if (!hasMotionDetection) 
-    changeColors();
-};
